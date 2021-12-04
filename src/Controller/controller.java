f34018da7d183c820c86ab87af544301ee39d246
tigero456 <tigero456@naver.com>
2021-12-04 18:56:45 +0900

    컨트롤러 생성

---
+++ src/Controller/controller.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import java.net.Socket;
+
+import ClientModel.clientDAO;
+import VO.loginVO;
+
+public class controller {
+	clientDAO c;
+	public controller(Socket socket){
+		c = new clientDAO(socket);
+	}
+	
+	public int login(String id, String pw){
+		loginVO lov = new loginVO("0", id, pw);
+		int i=c.checkIdPw(lov);
+		
+		
+		return i;
+	}
+}
+
