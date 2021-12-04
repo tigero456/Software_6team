9fac1ed83d430cef89c22f17b94515e621789365
sangin1 <tkwk9gh@naver.com>
2021-12-04 16:09:05 +0900

    VO생성

---
+++ src/VO/duesVO.java
@@ -0,0 +1,60 @@
+package VO;
+
+import java.io.Serializable;
+
+public class duesVO implements Serializable{
+	private String dues_id;
+	private String dues_name;
+	private String dues;
+	private String dues_date;
+	private String login_id;
+	
+	public void duesVO(String dues_id,String dues_name,String dues,String dues_date,String login_id) {
+		this.dues_id = dues_id;
+		this.dues_name = dues_name;
+		this.dues = dues;
+		this.dues_date = dues_date;
+		this.login_id = login_id;
+	}
+
+	public String getDues_id() {
+		return dues_id;
+	}
+
+	public void setDues_id(String dues_id) {
+		this.dues_id = dues_id;
+	}
+
+	public String getDues_name() {
+		return dues_name;
+	}
+
+	public void setDues_name(String dues_name) {
+		this.dues_name = dues_name;
+	}
+
+	public String getDues() {
+		return dues;
+	}
+
+	public void setDues(String dues) {
+		this.dues = dues;
+	}
+
+	public String getDues_date() {
+		return dues_date;
+	}
+
+	public void setDues_date(String dues_date) {
+		this.dues_date = dues_date;
+	}
+
+	public String getLogin_id() {
+		return login_id;
+	}
+
+	public void setLogin_id(String login_id) {
+		this.login_id = login_id;
+	}
+	
+}
---
+++ src/VO/loginVO.java
@@ -0,0 +1,34 @@
+package VO;
+
+import java.io.Serializable;
+
+public class loginVO implements Serializable{
+	private String login_id;
+	private String id;
+	private String pw;
+	
+	public void loginVO(String login_id,String id,String pw) {
+		this.login_id = login_id;
+		this.id = id;
+		this.pw = pw;
+	}
+	
+	public String getLogin_id() {
+		return login_id;
+	}
+	public void setLogin_id(String login_id) {
+		this.login_id = login_id;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+}
